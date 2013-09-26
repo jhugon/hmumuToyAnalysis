1f07cf262644d8f9583d7a11e5dd9ce089d61d79
Justin Hugon <jhugon@archer.ihepa.ufl.edu>
2013-09-26 07:07:28 -0400

    added analyzer.C

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ hmumuToyAnalysis
 ================
 
 H->mumu Ntuple example analyzer code
+
+Data samples are available on the UF analysis cluster in
+
+/raid/raid9/jhugon/higgsSamples/stage1/
+
+which physically resides on the archer node.
---
+++ analyzer.C
@@ -0,0 +1,140 @@
+
+#include <TSystem.h>
+#include <TROOT.h>
+#include <TClonesArray.h>
+#include <TParticle.h>
+#include <iostream>
+#include <TFile.h>
+#include <TTree.h>
+#include <TChain.h>
+#include <TH2F.h>
+#include <TH1F.h>
+#include <TProfile.h>
+#include <TCanvas.h>
+#include <TStyle.h>
+#include <TLegend.h>
+#include <TF1.h>
+#include <TRandom3.h>
+
+void analyzer ()
+{
+  using namespace std;
+
+  ///////////////////
+  // Configuration
+
+  unsigned maxEvents = 1000;
+  bool isData = false;
+
+  TString inputFileName("/raid/raid9/jhugon/higgsSamples/stage1/GluGlu_HToMM_M-125.root");
+
+  //gErrorIgnoreLevel = kError;
+
+  ///////////////////////////
+  Double_t MASS_MUON = 0.105658367;    //GeV/c2
+
+  //////////////////////////
+  // Tree Branches
+
+  TChain * tree = new TChain("tree");
+  tree->Add(inputFileName);
+
+  _MuonInfo reco1, reco2;
+
+  tree->SetBranchAddress("reco1", &reco1);
+  tree->SetBranchAddress("reco2", &reco2);
+
+  float recoCandMass, recoCandPt, recoCandY, recoCandPhi;
+  float recoCandMassRes, recoCandMassResCov;
+
+  tree->SetBranchAddress("recoCandMass",       &recoCandMass);
+  tree->SetBranchAddress("recoCandPt",         &recoCandPt);
+  tree->SetBranchAddress("recoCandY",          &recoCandY);
+  tree->SetBranchAddress("recoCandPhi",        &recoCandPhi);
+  tree->SetBranchAddress("recoCandMassRes",    &recoCandMassRes);
+  tree->SetBranchAddress("recoCandMassResCov", &recoCandMassResCov);
+
+  float trueMass=-99999.0;
+  if(!isData && tree->GetBranchStatus("trueMass"))
+    tree->SetBranchAddress("trueMass", &trueMass);
+
+  /// Higgs Boson 
+  _genPartInfo genHpostFSR;
+  if(!isData && tree->GetBranchStatus("genHpostFSR"))
+    tree->SetBranchAddress("genHpostFSR", &genHpostFSR);
+
+  _TrackInfo reco1GenPostFSR;
+  if(!isData && tree->GetBranchStatus("genM1HpostFSR"))
+    tree->SetBranchAddress("genM1HpostFSR", &reco1GenPostFSR);
+
+  _TrackInfo reco2GenPostFSR;
+  if(!isData && tree->GetBranchStatus("genM2HpostFSR"))
+    tree->SetBranchAddress("genM2HpostFSR", &reco2GenPostFSR);
+
+  _PFJetInfo jets;
+  tree->SetBranchAddress("pfJets",&jets);
+
+  float puJetFullDisc[10];
+  float puJetSimpleDisc[10];
+  float puJetCutDisc[10];
+
+  tree->SetBranchAddress("puJetFullDisc",&puJetFullDisc);
+  tree->SetBranchAddress("puJetSimpleDisc",&puJetSimpleDisc);
+  tree->SetBranchAddress("puJetCutDisc",&puJetCutDisc);
+
+  float puJetFullId[10];
+  float puJetSimpleId[10];
+  float puJetCutId[10];
+
+  tree->SetBranchAddress("puJetFullId",&puJetFullId);
+  tree->SetBranchAddress("puJetSimpleId",&puJetSimpleId);
+  tree->SetBranchAddress("puJetCutId",&puJetCutId);
+
+  int nPU=0;
+  if (!isData)
+    {
+      tree->SetBranchAddress("nPU",&nPU);
+    }
+  _VertexInfo vertexInfo;
+  tree->SetBranchAddress("vertexInfo",&vertexInfo);
+  _EventInfo eventInfo;
+  tree->SetBranchAddress("eventInfo",&eventInfo);
+  _MetInfo met;
+  tree->SetBranchAddress("met",&met);
+
+
+//  //////////////////////////
+//  //for PU reweighting
+//
+//  TRandom3 random(1457);
+//
+//  reweight::LumiReWeighting lumiWeights("pileupDists/PileUpHistMC2012Summer50nsPoissonOOTPU.root","pileupDists/PileUpHist2012ABCD.root","pileup","pileup");
+//  if (runPeriod == "7TeV")
+//    {
+//      cout << "Using 2011AB PU reweighting\n";
+//      lumiWeights = reweight::LumiReWeighting("pileupDists/PileUpHistMCFall11.root","pileupDists/PileUpHist2011AB.root","pileup","pileup");
+//    }
+//  else
+//    {
+//      cout << "Using 2012ABCD PU reweighting\n";
+//    }
+
+
+  unsigned nEvents = tree->GetEntries();
+  unsigned reportEach=1000;
+  if (nEvents/1000>reportEach)
+    reportEach = nEvents/1000;
+
+  for(unsigned i=0; i<nEvents;i++)
+  {
+    if(i >= maxEvents)
+      continue;
+
+    tree->GetEvent(i);
+    if (i % reportEach == 0) cout << "Event: " << i << endl;
+
+    cout << "recoCandMass: " << recoCandMass << endl;
+
+  }
+
+}
