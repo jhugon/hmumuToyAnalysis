b2b6c8243c27bbefe837c2932302bbf5d23faea6
Justin Hugon <jhugon@melrose.ihepa.ufl.edu>
2013-09-26 08:24:11 -0400

    added signal muon momentum smearing

---
+++ SmearingTool2011.h
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <TChain.h>
+#include <TClonesArray.h>
+#include <TString.h>
+#include <map>
+#include <TSystem.h>
+#include <TROOT.h>
+#include <TMath.h>
+#include <TLorentzVector.h>
+#include <TRandom3.h>
+#include "TF1.h"
+#include <stdio.h>
+#include <math.h>
+// class init
+class SmearingTool2011 {
+ public:
+  //SmearingTool2011();
+  //SmearingTool2011(int seed);
+  SmearingTool2011();
+  float PTsmear(float PTmuonGen, float ETAmuonGen, float CHARGEmuonGen, float PTmuonReco, int Ismear, float ParSig = 0, TString ParVar = "null");
+
+ private:
+  static const float mean[104];
+  static const float sig1[104];
+  static const float sig2[104];
+  static const float Asig2[104];
+  static const float ERRmean[104];
+  static const float ERRsig1[104];
+  static const float ERRsig2[104];
+  static const float ERRAsig2[104];
+  static const float ResRMS[104];
+  static const float ErrResRMS[104];
+
+  static const float meanMuPlus[104];
+  static const float sig1MuPlus[104];
+  static const float sig2MuPlus[104];
+  static const float Asig2MuPlus[104];
+  static const float ERRmeanMuPlus[104];
+  static const float ERRsig1MuPlus[104];
+  static const float ERRsig2MuPlus[104];
+  static const float ERRAsig2MuPlus[104];
+  static const float ResRMSMuPlus[104];
+  static const float ErrResRMSMuPlus[104];
+
+  static const float PTbin[14];
+  static const float ETAbin[9];
+
+};
+// end: class init
+
--- analyzer.C
+++ analyzer.C
@@ -19,6 +19,8 @@
 #include "DataFormats.h"
 #include "helpers.h"
 #include "LumiReweightingStandAlone.h"
+#include "SmearingTool.h"
+#include "SmearingTool2011.h"
 
 void analyzer ()
 {
@@ -29,6 +31,7 @@ void analyzer ()
 
   unsigned maxEvents = 100;
   bool isData = false;
+  bool isSignal = true; // set to true if Higgs signal
 
   TString inputFileName("/raid/raid8/jhugon/higgsSamples/stage1/8TeV/GluGlu_HToMM_M-125.root");
   TString runPeriod("8TeV");
@@ -37,6 +40,7 @@ void analyzer ()
   float maxMmm = 160;
 
   //gErrorIgnoreLevel = kError;
+  unsigned ISMEAR = 2;
 
   ///////////////////////////
   Double_t MASS_MUON = 0.105658367;    //GeV/c2
@@ -142,6 +146,16 @@ void analyzer ()
       muonIdFuncPtr = &isKinTight_2012_noIso;
     }
 
+  /////////////////////////
+  // Smearing
+  SmearingTool *smearPT = new SmearingTool();
+  SmearingTool2011 *smearPT2011 = new SmearingTool2011();
+
+  const double SQRT2 = sqrt(2);
+
+  /////////////////////////////
+  /////////////////////////////
+
   unsigned nEvents = tree->GetEntries();
   unsigned reportEach=1000;
   if (nEvents/1000>reportEach)
@@ -160,10 +174,54 @@ void analyzer ()
     tree->GetEvent(i);
     if (i % reportEach == 0) cout << "Event: " << i << endl;
 
+    // Reject events with invalid muons
+    if (reco1.pt < 0. || reco2.pt < 0.)
+        continue;
+
+    /////////////////////////////////////////////////
+    // Muon Resolution Smearing to match MuScleFit
+
+    if(isSignal) // smear only signal because it has muons from higgs 
+    {
+      if(reco1GenPostFSR.pt<0.)
+        cout << "Muon 1 Post FSR not valid!\n";
+      if(reco2GenPostFSR.pt<0.)
+        cout << "Muon 2 Post FSR not valid!\n";
+      float ptReco1 = -1.;
+      float ptReco2 = -1.;
+      if(runPeriod == "7TeV")
+      {
+        ptReco1 = smearPT2011 -> PTsmear(reco1GenPostFSR.pt, reco1GenPostFSR.eta, reco1GenPostFSR.charge, reco1.pt, ISMEAR);
+        ptReco2 = smearPT2011 -> PTsmear(reco2GenPostFSR.pt, reco2GenPostFSR.eta, reco2GenPostFSR.charge, reco2.pt, ISMEAR);
+      }
+      else
+      {
+        ptReco1 = smearPT -> PTsmear(reco1GenPostFSR.pt, reco1GenPostFSR.eta, reco1GenPostFSR.charge, reco1.pt, ISMEAR);
+        ptReco2 = smearPT -> PTsmear(reco2GenPostFSR.pt, reco2GenPostFSR.eta, reco2GenPostFSR.charge, reco2.pt, ISMEAR);
+      }
+      TLorentzVector reco1Vec;
+      TLorentzVector reco2Vec;
+      reco1Vec.SetPtEtaPhiM(ptReco1,reco1.eta,reco1.phi,MASS_MUON);
+      reco2Vec.SetPtEtaPhiM(ptReco2,reco2.eta,reco2.phi,MASS_MUON);
+      TLorentzVector diMuonVec = reco1Vec + reco2Vec;
+
+      reco1.pt = ptReco1;
+      reco2.pt = ptReco2;
+      recoCandMass = diMuonVec.M();
+      recoCandPt = diMuonVec.Pt();
+      recoCandY = diMuonVec.Rapidity();
+      recoCandPhi = diMuonVec.Phi();
+    
+      reco1Vec.SetPtEtaPhiM(ptReco1,reco1.eta,reco1.phi,MASS_MUON);
+      reco2Vec.SetPtEtaPhiM(ptReco2,reco2.eta,reco2.phi,MASS_MUON);
+      diMuonVec = reco1Vec + reco2Vec;
+    }
+
+    //////////////////////////////////////////
     // Muon-related cuts
 
-    //if (recoCandMass > maxMmm || recoCandMass < minMmm)
-    //    continue;
+    if (recoCandMass > maxMmm || recoCandMass < minMmm)
+        continue;
 
     bool muon1PassId = (*muonIdFuncPtr)(reco1);
     bool muon2PassId = (*muonIdFuncPtr)(reco2);
@@ -174,6 +232,14 @@ void analyzer ()
     bool muon2PassIso = (getPFRelIso(reco2) <= 0.12);
     if ( !(muon1PassIso && muon2PassIso))
         continue;
+
+    // Order muons by pt
+    if (reco1.pt < reco2.pt)
+    {
+      _MuonInfo tmpMuon = reco1;
+      reco1 = reco2;
+      reco1 = tmpMuon;
+    }
 
     // PU reweight
     float weight = 1.;
