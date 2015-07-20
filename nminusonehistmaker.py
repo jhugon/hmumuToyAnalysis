762c6b107b54c2709a78464f05b776f049256e9e
Justin <jhugon@cern.fake>
2015-07-20 14:56:53 -0400

    nminusonehistmaker.py: should work now

--- nminusonehistmaker.py
+++ nminusonehistmaker.py
@@ -87,24 +87,74 @@ def loadFiles(globstr):
         
   return results
 
+def nminusoneCutMaker(variableStr,cutStr):
+  result = ""
+  cutList = cutStr.split("&&")
+  for i, cut in enumerate(cutList):
+    if variableStr in cut:
+      cutList.pop(i)
+  result = " && ".join(cutList)
+  return result
+
 def makeHists(fileDict):
+  print "Running on {0}".format(fileDict['fn'])
   tfile = root.TFile(fileDict['fn'])
   tree = tfile.Get("treefriend")
+  tree.AddBranchToCache("*")
+  
   #tree.Print()
 
   outfile = root.TFile(fileDict['fnOut'],"RECREATE")
   outfile.cd()
 
+  genericMuonCuts = "muon.isGlobal && muon.isPFMuon && muon.pt > 15 && abs(muon.eta) < 2.1 & muon.numTrackerLayers >= 6 && abs(muon.d0_PV) <= 0.2 && abs(muon.dz_PV) <= 0.5 && muon.numValidMuonHits >= 1 && muon.numValidPixelHits >= 1 && muon.numOfMatchedStations >= 2 && muon.normChiSquare <= 10 && (muon.sumChargedHadronPtR04 + max(0.0,muon.sumNeutralHadronEtR04+muon.sumPhotonEtR04 - 0.5*muon.sumPUPtR04))/muon.pt <= 0.12"
+  triggerMatchedCut = "(( muLead.isHltMatched[0] && muLead.pt > 25.0 ) || ( muSubLead.isHltMatched[0] && muSubLead.pt > 25.0 ))"
+  muonCuts = genericMuonCuts.replace("muon","muLead") + " && " + genericMuonCuts.replace("muon","muSubLead")
+
   histDicts = [
-    {'name':"muLeadPt",'var':"muLead.pt",'hist':"300,0,300",'cuts':'muSubLead.pt>15.'},
-    {'name':"muSubLeadPt",'var':"muSubLead.pt",'hist':"300,0,300",'cuts':'muLead.pt>25.'},
+    {'name':"muLeadPt",'var':"muLead.pt",'hist':"300,0,300"},
+    {'name':"muSubLeadPt",'var':"muSubLead.pt",'hist':"300,0,300"},
+    {'name':"muLeadIsGlobal",'var':"muLead.isGlobal",'hist':"2,-0.5,1.5"},
+    {'name':"muSubLeadIsGlobal",'var':"muSubLead.isGlobal",'hist':"2,-0.5,1.5"},
+    {'name':"muLeadIsPFMuon",'var':"muLead.isPFMuon",'hist':"2,-0.5,1.5"},
+    {'name':"muSubLeadIsPFMuon",'var':"muSubLead.isPFMuon",'hist':"2,-0.5,1.5"},
+    {'name':"muLeadEta",'var':"muLead.eta",'hist':"100,-2.5,2.5"},
+    {'name':"muSubLeadEta",'var':"muSubLead.eta",'hist':"100,-2.5,2.5"},
+    {'name':"muLeadNumTrackerLayers",'var':"muLead.numTrackerLayers",'hist':"21,-0.5,20.5"},
+    {'name':"muSubLeadNumTrackerLayers",'var':"muLead.numTrackerLayers",'hist':"21,-0.5,20.5"},
+    {'name':"muLeadD0",'var':"muLead.d0_PV",'hist':"100,-0.2,0.2"},
+    {'name':"muSubLeadD0",'var':"muLead.d0_PV",'hist':"100,-0.2,0.2"},
+    {'name':"muLeadDz",'var':"muLead.dz_PV",'hist':"100,-0.2,0.2"},
+    {'name':"muSubLeadDz",'var':"muLead.dz_PV",'hist':"100,-0.2,0.2"},
+    {'name':"muLeadNumValidMuonHits",'var':"muLead.numValidMuonHits",'hist':"21,-0.5,20.5"},
+    {'name':"muSubLeadNumValidMuonHits",'var':"muLead.numValidMuonHits",'hist':"21,-0.5,20.5"},
+    {'name':"muLeadNumValidPixelHits",'var':"muLead.numValidPixelHits",'hist':"21,-0.5,20.5"},
+    {'name':"muSubLeadNumValidPixelHits",'var':"muLead.numValidPixelHits",'hist':"21,-0.5,20.5"},
+    {'name':"muLeadNumOfMatchedStations",'var':"muLead.numOfMatchedStations",'hist':"5,-0.5,4.5"},
+    {'name':"muSubLeadNumOfMatchedStations",'var':"muLead.numOfMatchedStations",'hist':"5,-0.5,4.5"},
+    {'name':"muLeadNormChiSquare",'var':"muLead.normChiSquare",'hist':"250,0,5.0"},
+    {'name':"muSubLeadNormChiSquare",'var':"muLead.normChiSquare",'hist':"250,0,5.0"},
+    {'name':"muLeadIsHltMatched",'var':"muLead.isHltMatched[0]",'hist':"2,-0.5,1.5"},
+    {'name':"muSubLeadIsHltMatched",'var':"muSubLead.isHltMatched[0]",'hist':"2,-0.5,1.5"},
   ]
 
+  nminusoneHistDicts = []
   for ihist, histDict in enumerate(histDicts):
+    histDict['cuts'] = nminusoneCutMaker(histDict['var'],muonCuts) +" && "+ triggerMatchedCut
+    nminusoneHistDicts.append(histDict)
 
+  nminusoneHistDicts.extend([
+    {'name':"muLeadPFRelIso",'var':"(muLead.sumChargedHadronPtR04 + max(0.0,muLead.sumNeutralHadronEtR04+muLead.sumPhotonEtR04 - 0.5*muLead.sumPUPtR04))/muLead.pt",'hist':"200,0,2",'cuts':nminusoneCutMaker("muLead.sumNeutralHadronEtR04",muonCuts)+" && "+ triggerMatchedCut},
+    {'name':"muSubLeadPFRelIso",'var':"(muSubLead.sumChargedHadronPtR04 + max(0.0,muSubLead.sumNeutralHadronEtR04+muSubLead.sumPhotonEtR04 - 0.5*muSubLead.sumPUPtR04))/muSubLead.pt",'hist':"200,0,2",'cuts':nminusoneCutMaker("muSubLead.sumNeutralHadronEtR04",muonCuts)+" && "+ triggerMatchedCut},
+    {'name':"muLeadIsHltMatched",'var':"muLead.isHltMatched[0]",'hist':"2,-0.5,1.5",'cuts':muonCuts + "&& muSubLead.isHltMatched[0] && muSubLead.pt>25.0"},
+    {'name':"muSubLeadIsHltMatched",'var':"muSubLead.isHltMatched[0]",'hist':"2,-0.5,1.5",'cuts':muonCuts + "&& muLead.isHltMatched[0] && muLead.pt>25.0"},
+  ])
+
+  for ihist, histDict in enumerate(nminusoneHistDicts):
     histTmpName = "hist{0}".format(ihist)
 
     histName = histDict['name']
+    print "  Making Histogram of '{0}'".format(histName)
     histVariableString = histDict['var']
     histDrawString = " >> {0}({1})".format(histTmpName,histDict['hist'])
     histCutString = histDict['cuts']
@@ -112,12 +162,13 @@ def makeHists(fileDict):
     tree.Draw(histVariableString+histDrawString,histCutString)
     hist = root.gDirectory.Get(histTmpName)
     hist.SetName(histName)
+    hist.SetTitle("")
     hist.Write()
 
   outfile.Close()
-  sys.exit(0)
 
 if __name__ == "__main__":
+
 
   globstr = "*_ptOrdered.root"
   fileDicts = loadFiles(globstr)
